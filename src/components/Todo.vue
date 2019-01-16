e5cbb49c50ffe6738b0ad71fb627959e156f8280
Justin Koehler <18156271+jdkoeh79@users.noreply.github.com>
2019-01-15 23:53:35 -0600

    the start of a todo-component

---
+++ src/components/Todo.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="todo">
+    <div class="title-bar">
+      <v-checkbox
+        height="0"
+        color="#00897b"
+        v-model="todo.completed">
+      </v-checkbox>
+
+      <div class="todo-title">
+        {{ todo.title }}
+      </div>
+
+      <div class="due-date">
+        {{ todo.dueDate }} {{todo.dueTime }}
+      </div>
+
+      <v-icon class="remove-button">close</v-icon>
+     </div> <!-- title-bar -->
+     <div class="todo-body">
+       {{ todo.priority }}
+     </div>
+  </div> <!-- todo -->
+</template>
+
+<script>
+export default {
+  props: [
+    'todo'
+  ]
+}
+</script>
+
+<style scoped>
+.todo {
+
+  text-align: left;
+  padding: 10px;
+  border-bottom: 1px solid rgb(176, 212, 200);
+}
+
+.title-bar {
+  display: flex;
+}
+
+.title-bar div {
+  flex-grow: 0;
+}
+
+.todo .todo-title {
+  flex-grow: 3;
+}
+
+.todo-title {
+  font-size: 18px;
+  padding-left: 5px;
+}
+
+.due-date {
+  flex-grow: 0;
+  font-size: 1.2em;
+}
+
+.remove-button {
+  flex-grow: 0;
+  padding-left: 10px;
+}
+
+.todo-body {
+  margin-left: 38px;
+  font-size: 12px;
+}
+
+>>>.v-input {
+  flex: none;
+}
+
+>>>.v-messages {
+  min-height: 0;
+}
+
+>>>.v-input--selection-controls {
+  padding-top: 0;
+  margin-top: 13px;
+}
+</style>
--- src/views/Todos.vue
+++ src/views/Todos.vue
@@ -1,24 +1,132 @@
 <template>
   <v-layout>
-    <v-flex>
+    <v-flex xs8 mr-4>
+      <div class="todo-container">
+        <todo
+          v-for="todo in todos"
+          :key="todo.id"
+          :todo="todo"
+        />
+        <div class="add-todo">
+          <v-icon class="add-button">add</v-icon>
+            New To-Do
+        </div>
+      </div>
+    </v-flex>
+    <v-flex xs4>
+      <div class="white">
+        <ul>
+          <li
+            v-for="(item, i) in todos[0].items"
+            :index="i"
+            :key="i"
+          >
+            {{ item }}
+          </li>
+        </ul>
+      </div>
     </v-flex>
   </v-layout>
 </template>
 
 <script>
 import { mapState } from 'vuex'
+import Todo from '../components/Todo.vue'
+// import TodoItems from '../components/TodoItems.vue'
 
 export default {
-  name: "My Todo's",
+  name: 'Todos',
   computed: {
     ...mapState([
       'isUserLoggedIn',
       'user'
     ])
+  },
+  components: {
+    Todo
+    // TodoItems
+  },
+  data () {
+    return {
+      todos: [
+        {
+          id: 1,
+          title: 'Go Grocery Shopping',
+          completed: false,
+          priority: 'Normal',
+          dueDate: null,
+          dueTime: null,
+          items: [
+            'Milk',
+            'Eggs',
+            'Bread',
+            'Peanut Butter',
+            'Cereal',
+            'Oranges',
+            'Bananas',
+            'Steak',
+            'Beer'
+          ]
+        },
+        {
+          id: 2,
+          title: 'Edit Christmas Footage',
+          completed: false,
+          priority: 'Low',
+          dueDate: null,
+          dueTime: null,
+          items: []
+        },
+        {
+          id: 3,
+          title: 'Management Meeting',
+          completed: false,
+          priority: 'High',
+          dueDate: '1/21/219',
+          dueTime: '3:30 PM',
+          items: []
+        },
+        {
+          id: 4,
+          title: 'Pick up dry-cleaning',
+          completed: false,
+          priority: 'Low',
+          dueDate: null,
+          dueTime: null,
+          items: []
+        },
+        {
+          id: 5,
+          title: 'Pay Electric Bill',
+          completed: false,
+          priority: 'Normal',
+          dueDate: null,
+          dueTime: null,
+          items: []
+        }
+      ]
+    }
   }
 }
 </script>
 
 <style scoped>
+.todo-container {
+  background-color: white;
+  padding: 0 10px 10px;
+}
+
+.add-todo {
+  color: grey;
+  font-size: 16px;
+  text-align: left;
+  padding: 10px;
+}
+
+.add-button {
+  position: relative;
+  top: 3px;
+  padding-right: 5px;
+}
 
 </style>
